e5723b0744350f192092b96d088feb3fcbc57f4a
Tilak Bhandari <tilakb1989@gmail.com>
2019-03-06 12:16:17 +0530

    intial commit

---
+++ precache-manifest.a1d729c10ad3c167b048338c0d0282c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd9813858b09af7222df",
+    "url": "js/chunk-vendors-legacy.475f8607.js"
+  },
+  {
+    "revision": "fd78b04f2de1fd9274f1",
+    "url": "js/app-legacy.fe10202f.js"
+  },
+  {
+    "revision": "48af24cd7dae7280ac56",
+    "url": "js/about-legacy.41d636ee.js"
+  },
+  {
+    "revision": "f5be3af71d97f1ea569deaf50e9ab50a",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "fd78b04f2de1fd9274f1",
+    "url": "css/app.4146caab.css"
+  }
+];
---
+++ precache-manifest.e5fcbeb52c7b0017ab160bae5bb674b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "dafed3cc861fcf2e29ed",
+    "url": "js/chunk-vendors.0e0eca66.js"
+  },
+  {
+    "revision": "f4d35570bdafe03cf70a",
+    "url": "js/app.aa58eb69.js"
+  },
+  {
+    "revision": "48af24cd7dae7280ac56",
+    "url": "js/about.41d636ee.js"
+  },
+  {
+    "revision": "59b4c74533db94f6c276a0751182b8bf",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f4d35570bdafe03cf70a",
+    "url": "css/app.4146caab.css"
+  }
+];
